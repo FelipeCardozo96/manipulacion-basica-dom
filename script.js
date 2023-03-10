965f4623612ebafbaf9e58f25baff543b92b3395
FelipeCardozo96 <felipemcr996@gmail.com>
2023-03-10 14:02:36 -0300

    asd

---
+++ script.js
@@ -0,0 +1,12 @@
+const h1 = document.querySelector('h1');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+let resultado = document.querySelector('#resultado')
+
+/* btn.addEventListener('click', botonCalcular)
+
+function botonCalcular() {
+    const pResult = input1.value + input2.value
+   resultado.innerHTML = 'Resultado : ' + pResult
+} */
